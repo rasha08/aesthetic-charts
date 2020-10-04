0024bb564e0fddb40c8932fe51036afacecf744a
rasha08 <rasha08@gmail.com>
2020-10-04 12:17:41 +0200

    Updates

---
+++ precache-manifest.95b2443b94302fe575e2efef22f443f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175c4fa494809f917738a9ebc6300bfb",
+    "url": "/aesthetic-charts/index.html"
+  },
+  {
+    "revision": "3e8636e679b95fbd8f57",
+    "url": "/aesthetic-charts/static/css/main.1aa0a849.chunk.css"
+  },
+  {
+    "revision": "df84631bb73c8951d03c",
+    "url": "/aesthetic-charts/static/js/2.704dcb60.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/aesthetic-charts/static/js/2.704dcb60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8636e679b95fbd8f57",
+    "url": "/aesthetic-charts/static/js/main.4dda5113.chunk.js"
+  },
+  {
+    "revision": "08819ea349eb9f66f10d",
+    "url": "/aesthetic-charts/static/js/runtime-main.fa236eef.js"
+  }
+]);
